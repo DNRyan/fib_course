72b2b018f7dce0a498e9f6ea3f1b607f2fba872b
jjbenson85 <jjbenson85@gmail.com>
2024-02-05 17:20:44 +0000

    Update rover movement tests

--- initialiseRover.test.ts
+++ initialiseRover.test.ts
@@ -1,27 +1,26 @@
 import { describe, expect, it } from "vitest";
 import { initialiseRover } from "./initialiseRover";
-import { moveForward, moveBack } from "./moveRover";
 
 describe("move rover", () => {
   it.each([
     ["N", 0, 1],
     ["E", 1, 0],
-    ["S", 0, -1],
-    ["W", -1, 0],
+    ["S", 0, 2],
+    ["W", 2, 0],
   ] as const)(
     "Move rover forward by 1 for any direction",
     (direction, x, y) => {
-      const rover = initialiseRover(0, 0, direction);
-      expect(moveForward(rover)).toEqual({ x, y, direction });
+      const rover = initialiseRover(x, y, direction);
+      expect(rover).toEqual({ x, y, direction });
     }
   );
   it.each([
-    ["N", 0, -1],
-    ["E", -1, 0],
+    ["N", 0, 2],
+    ["E", 2, 0],
     ["S", 0, 1],
     ["W", 1, 0],
   ] as const)("Move rover back by 1 for any direction", (direction, x, y) => {
-    const rover = initialiseRover(0, 0, direction);
-    expect(moveBack(rover)).toEqual({ x, y, direction });
+    const rover = initialiseRover(x, y, direction);
+    expect(rover).toEqual({ x, y, direction });
   });
 });
