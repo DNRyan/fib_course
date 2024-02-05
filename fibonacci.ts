489e8e4bee22b3d409f3e6d0f0ef41ba052b1291
Daniel Ryan <31455292+DNRyan@users.noreply.github.com>
2024-02-05 14:26:52 +0000

    Remove recursion

--- fibonacci.ts
+++ fibonacci.ts
@@ -1,3 +1,18 @@
 export function fibonacci(n: number) {
-  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+  if (n < 2) {
+    return n
+  }
+
+  let fib_minus_one = 1
+  let fib_minus_two = 0
+  let fib = 1
+
+  for (let i = 2; i <=n; i++) {
+    const result = fib_minus_one + fib_minus_two
+    fib_minus_two = fib_minus_one
+    fib_minus_one = fib
+    fib = result
+  }
+
+  return fib
 }
